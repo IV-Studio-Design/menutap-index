53cc23dd116abae474646da38fcf7aca148dc1f1
= <thales.mariano125@gmail.com>
2025-01-12 15:41:56 -0300

    feat: add AppHeader component

---
+++ components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="w-full flex justify-center items-center shadow-md">
+        <header class="w-full max-w-5xl h-full py-6 flex justify-between items-center">
+            <img class="w-28" src="@/assets/images/menutap_logo.png" />
+            <nav>
+                <ul class="flex gap-5 items-center">
+                    <li>
+                        <NuxtLink class="app-header-link" exactActiveClass="font-bold" to="/">
+                            INICIO
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink class="app-header-link" exactActiveClass="font-bold">
+                            RECURSOS
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink class="app-header-link" exactActiveClass="font-bold">
+                            PLANOS E PREÇOS
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink class="app-header-link" exactActiveClass="font-bold">
+                            ÁREA DO CLIENTE
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink class="app-header-link free-version-link" exactActiveClass="font-bold">
+                            VERSÃO GRÁTIS
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+    .free-version-link {
+        @apply font-bold text-tapOrange border-2 border-tapOrange rounded-md px-5 py-2
+    }
+    .app-header-link {
+        @apply font-raleway text-sm
+    }
+</style>
