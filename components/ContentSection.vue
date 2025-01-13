c72a82bfd5f9cf5ffd6722282b95e6ca22c3cb2b
= <thales.mariano125@gmail.com>
2025-01-13 11:47:34 -0300

    feat: add ContentSection component

---
+++ components/ContentSection.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-full flex justify-center items-center bg-tapOrangeLight bg-center bg-cover">
+        <section class="w-full max-w-5xl flex gap-8 justify-center items-center py-24">
+            <div class="flex gap-10 justify-center items-center">
+                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
+                    <div class="w-20 h-20 bg-tapOrangeLight absolute -top-10 flex justify-center items-center rounded-full">
+                        <IconsReader width="52" height="52" fill="#fff" />
+                    </div>
+                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                        <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
+                            Ponto de venda <br>
+                            e cardápio digital
+                        </h3>
+                        <p class="font-raleway text-center leading-5">
+                            Gerencie todos os <br>
+                            aspectos do seu <br>
+                            negócio em uma <br>
+                            <strong>
+                                única plataforma.
+                            </strong>
+                        </p>
+                    </div>
+                    
+                </div>
+                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
+                    <div class="w-20 h-20 bg-tapOrangeLight absolute -top-10 flex justify-center items-center rounded-full">
+                        <IconsWallet width="52" height="52" fill="#fff" />
+                    </div>
+                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                        <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
+                            Seu Delivery, <br>
+                            Sem Taxas.
+                        </h3>
+                        <p class="font-raleway text-center leading-5">
+                            Tenha <strong>seu próprio</strong> <br>
+                            site delivery <strong>sem taxas.</strong> <br>
+                            Comece grátis!
+                        </p>
+                    </div>
+                </div>
+                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
+                    <div class="w-20 h-20 bg-tapOrangeLight absolute -top-10 flex justify-center items-center rounded-full">
+                        <IconsCheckmarkCircle width="52" height="52" fill="#fff" />
+                    </div>
+                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                        <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
+                           Interface Simples <br>
+                           e Intuitiva
+                        </h3>
+                        <p class="font-raleway text-center leading-5">
+                            <strong>
+                                Sem configurações <br>
+                                complexas.
+                            </strong> ou <br>
+                            processos cansativos <br>
+                            para você.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
     <AppHeader />
     <HeroSection />
+    <ContentSection />
 </template>
