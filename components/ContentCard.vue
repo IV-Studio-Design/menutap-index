4de3755aa0b92b5d5c1e7ee72a85403ae0a9b0a2
= <thales.mariano125@gmail.com>
2025-02-07 15:21:19 -0300

    feat: add ContentCard and some adjusts

---
+++ components/ContentCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-52 h-64 bg-[url('@/assets/images/card_bg.svg')] bg-cover bg-center flex justify-center relative rounded-2xl">
+        <div class="w-20 h-20 absolute -top-10 flex justify-center items-center rounded-full">
+            <slot name="icon" />
+        </div>
+        <div class="w-full h-full flex gap-4 flex-col justify-start items-center mt-3 pt-16">
+            <slot name="content" />
+        </div>           
+    </div>
+</template>
--- components/ContentSection.vue
+++ components/ContentSection.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="w-full bg-gradient-to-l to-tapOrangeLight
-     from-tapOrange bg-center bg-cover">
-     <div class="w-full flex justify-center items-center bg-[url('@/assets/images/bg2.png')]  bg-cover bg-center">
+    <div class="w-full bg-gradient-to-l to-[#ff9d46]
+     from-[#ff7f43]">
+     <div class="w-full flex justify-center items-center bg-[url('@/assets/images/bg2.png')] bg-cover bg-center">
         <section class="w-full max-w-5xl flex gap-8 justify-center items-center py-24">
             <div class="flex gap-10 justify-center items-center">
-                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
-                    <div class="w-20 h-20 bg-white shadow-lg absolute -top-10 flex justify-center items-center rounded-full">
-                        <IconsReader width="44" height="44" fill="#F8A931" />
-                    </div>
-                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                <ContentCard>
+                    <template #icon>
+                        <IconsReader width="56" height="56" fill="#fff" />
+                    </template>
+                    <template #content>
                         <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
                             Ponto de venda <br>
                             e cardápio digital
                         </h3>
-                        <p class="font-raleway text-center leading-5 text-sm">
+                        <p class="font-raleway text-center leading-4">
                             Gerencie todos os <br>
                             aspectos do seu <br>
                             negócio em uma <br>
@@ -21,35 +21,34 @@
                                 única plataforma.
                             </strong>
                         </p>
-                    </div>
-                    
-                </div>
-                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
-                    <div class="w-20 h-20 bg-tapOrangeLight absolute -top-10 flex justify-center items-center rounded-full">
-                        <IconsWallet width="52" height="52" fill="#fff" />
-                    </div>
-                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                    </template>
+                </ContentCard>
+                <ContentCard>
+                    <template #icon>
+                        <IconsWallet width="56" height="56" fill="#fff" />
+                    </template>
+                    <template #content>
                         <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
                             Seu Delivery, <br>
                             Sem Taxas.
                         </h3>
-                        <p class="font-raleway text-center leading-5 text-sm">
+                        <p class="font-raleway text-center leading-4">
                             Tenha <strong>seu próprio</strong> <br>
                             site delivery <strong>sem taxas.</strong> <br>
                             Comece grátis!
                         </p>
-                    </div>
-                </div>
-                <div class="w-48 h-56 bg-white flex justify-center relative rounded-2xl">
-                    <div class="w-20 h-20 bg-tapOrangeLight absolute -top-10 flex justify-center items-center rounded-full">
-                        <IconsCheckmarkCircle width="52" height="52" fill="#fff" />
-                    </div>
-                    <div class="w-full h-full flex gap-2 flex-col justify-center items-center mt-3">
+                    </template>
+                </ContentCard>
+                <ContentCard>
+                    <template #icon>
+                        <IconsCheckmarkCircle width="56" height="56" fill="#fff" />
+                    </template>
+                    <template #content>
                         <h3 class="font-bold font-raleway text-tapOrange text-center text-lg leading-5">
                            Interface Simples <br>
                            e Intuitiva
                         </h3>
-                        <p class="font-raleway text-center leading-5 text-sm">
+                        <p class="font-raleway text-center leading-4">
                             <strong>
                                 Sem configurações <br>
                                 complexas.
@@ -57,8 +56,8 @@
                             processos cansativos <br>
                             para você.
                         </p>
-                    </div>
-                </div>
+                    </template>
+                </ContentCard>
             </div>
         </section>
      </div>
