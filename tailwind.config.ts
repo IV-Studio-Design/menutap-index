616e1792ac54c2c7c09fdde8cc8038fa7f5923ca
= <thales.mariano125@gmail.com>
2025-02-07 15:28:01 -0300

    feat: shadow-header opacity adjust

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default<Partial<Config>> {
                 raleway: ['Raleway', 'sans-serif']
             },
             boxShadow: {
-                'header': '0px 10px 15px rgba(0, 0, 0, .07)'
+                'header': '0px 10px 15px rgba(0, 0, 0, .05)'
             }
         }
     }
