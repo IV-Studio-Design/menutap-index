e4dad60c21cb9d6e76cfdf2ac5eddc7262b4b5b2
= <thales.mariano125@gmail.com>
2025-02-10 16:27:58 -0300

    feat: add responsivity to AppFooter

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full bg-tapOrange">
         <footer class="w-full h-full flex flex-col justify-center items-center">
-            <div class="w-full max-w-5xl flex justify-between items-center py-16">
-                <nav class="h-20 flex gap-3 items-start">
+            <div class="w-full max-w-5xl flex flex-col gap-10 lg:gap-0 lg:flex-row justify-center lg:justify-between items-center px-2 py-10 lg:py-16">
+                <nav class="h-20 flex gap-3 items-center lg:items-start">
                     <a><IconsFacebook width="20" height="20" fill="white"/></a>
                     <a><IconsYoutube width="20" height="20" fill="white"/></a>
                     <a><IconsInstagram width="20" height="20" fill="white"/></a>
@@ -10,7 +10,7 @@
                     <a><IconsLinkedin width="20" height="20" fill="white"/></a>
                     <a><IconsPinterest width="20" height="20" fill="white"/></a>
                 </nav>
-                <div class="space-y-5 border-x border-white px-10">
+                <div class="space-y-5 lg:border-x lg:border-white px-10 text-center lg:text-left">
                     <NuxtLink class="text-white font-raleway font-light">Blog da MenuTap</NuxtLink>
                     <p class="text-white font-bold text-sm font-raleway">
                         Dicas e insights para otimizar seu negócio e aprimorar a <br>
@@ -18,7 +18,7 @@
                     </p>
                 </div>
                 <div class="space-y-5">
-                    <h3 class="text-white font-raleway font-bold text-right">
+                    <h3 class="text-white font-raleway font-bold text-center lg:text-right">
                         Suporte e vendas <br>
                         Fale conosco pelo Whatsapp
                     </h3>
@@ -28,10 +28,10 @@
                     </p>
                 </div>
             </div>
-            <div class="w-full flex justify-center bg-white white py-5">
-                <p class="font-raleway pr-5 text-sm">&copy 2025 MenuTap Tecnologia</p>
-                <a class="font-raleway font-bold text-sm border-x border-black px-5">Termos de Uso</a>
-                <a class="font-raleway font-bold pl-5 text-sm">Política de Privacidade</a>
+            <div class="w-full flex flex-col gap-2 sm:gap-0 sm:flex-row justify-center items-center sm:items-start bg-white white py-5">
+                <p class="font-raleway sm:pr-5 text-sm">&copy 2025 MenuTap Tecnologia</p>
+                <a class="font-raleway font-bold text-sm sm:border-x sm:border-black px-5">Termos de Uso</a>
+                <a class="font-raleway font-bold sm:pl-5 text-sm">Política de Privacidade</a>
             </div>
         </footer>
     </div>
