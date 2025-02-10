9b1ab815701738fa2240a428db341ac66c3dd92a
= <thales.mariano125@gmail.com>
2025-02-10 15:28:47 -0300

    feat: add responsity to AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,8 +1,19 @@
+<script lang="ts" setup>
+    const showSideMenu = ref<boolean>(false);
+
+    onMounted(() => {
+        window.onresize = () => {
+            if(showSideMenu.value) showSideMenu.value = false
+        }
+    })
+
+</script>
+
 <template>
-    <div class="w-full flex justify-center items-center shadow-header">
-        <header class="w-full max-w-4xl h-full py-6 flex justify-between items-center">
+    <div class="w-full h-[72px] bg-white flex justify-center items-center shadow-header z-50 sticky top-0 left-0">
+        <header class="w-full max-w-4xl h-full px-2 py-6 flex justify-between items-center">
             <img class="w-28" src="@/assets/images/menutap_logo.png" />
-            <nav>
+            <nav class="hidden md:block">
                 <ul class="flex gap-7 items-center">
                     <li>
                         <NuxtLink class="app-header-link" exactActiveClass="font-bold" to="/">
@@ -31,7 +42,30 @@
                     </li>
                 </ul>
             </nav>
+
+            <div class="md:hidden">
+                <button @click="showSideMenu = !showSideMenu" class="space-y-1 mt-1">
+                    <div :class="showSideMenu ? 'button-bar rotate-45 translate-y-2' : 'button-bar'"></div>
+                    <div :class="showSideMenu ? 'button-bar translate-x-5 opacity-0' : 'button-bar'"></div>
+                    <div :class="showSideMenu ? 'button-bar -rotate-45 -translate-y-2' : 'button-bar'"></div>
+                </button>
+            </div>
         </header>
+
+        <Transition
+            enter-active-class="animate__animated animate__fadeIn"
+            leave-active-class="animate__animated animate__fadeOut"
+        >
+            <div v-show="showSideMenu" class="w-full h-full flex justify-end bg-neutral-900/30 fixed top-[72px] left-0">
+                <Transition
+                    enter-active-class="animate__animated animate__slideInRight"
+                    leave-active-class="animate__animated animate__slideOutRight"
+                >
+                    <SideMenu v-show="showSideMenu" />
+                </Transition>
+            </div>
+        </Transition>
+        
     </div>
 </template>
 
@@ -40,6 +74,10 @@
         @apply font-bold text-tapOrange border-2 border-tapOrange rounded-md px-5 py-2
     }
     .app-header-link {
-        @apply font-raleway text-sm
+        @apply font-raleway text-[13px]
+    }
+
+    .button-bar {
+        @apply w-7 h-1 bg-tapOrange rounded-lg transition-all duration-300
     }
 </style>
